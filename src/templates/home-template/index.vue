f9d329adb3f24f87e7bca420f0abacc2a7c6cfe1
Daniel Alves Bezerra <47247720+danialvesb@users.noreply.github.com>
2020-11-05 19:36:57 -0300

    adiciondo estrutura do projeto

---
+++ src/templates/home-template/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content">
+    <section class="header">
+      <slot name="header"></slot>
+    </section>
+    <section class="body">
+      <slot name="body"></slot>
+    </section>
+    <section class="fotter">
+      <slot name="fotter"></slot>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-template',
+};
+</script>
+
+<style>
+@import url("./styles.css");
+</style>
---
+++ src/views/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <home-template>
+    <div slot="header"></div>
+    <div slot="body"></div>
+  </home-template>
+</template>
+
+<script>
+import homeTemplate from '@/templates/home-template/index.vue';
+
+export default {
+  name: 'home',
+  components: {
+    homeTemplate,
+  },
+};
+</script>
+
+<style scoped>
+  @import url('./styles.css');
+</style>
